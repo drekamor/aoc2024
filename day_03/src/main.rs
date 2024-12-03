fd58e6495348a83223f1d200cfd46586f2dc7867
drekamor <ddrekamor@gmail.com>
2024-12-03 13:10:32 +0000

    clean up

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -2,8 +2,6 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use regex::Regex;
 
-//const reg = Regex::new(r"mul\(\d*,\d*\)").unwrap();
-
 fn main() {
     let file = File::open("./resources/input.in").unwrap();
     let mut reader = BufReader::new(file);
@@ -12,7 +10,6 @@ fn main() {
     reader.read_to_string(&mut input).unwrap();
    
     let reg_inst = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
-    // let reg_mul = Regex::new(r"mul\(\d+,\d+\)").unwrap();
     let reg_nums = Regex::new(r"(?<a>\d+),(?<b>\d+)").unwrap();
     let instructions: Vec<&str> = reg_inst.find_iter(&input).map(|m| m.as_str()).collect();
 
